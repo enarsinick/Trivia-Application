763b497835c751f7e8b0b8543362f275e1c9d295
Nick Cave <nxdmedia@googlemail.com>
2020-06-30 20:30:40 +0100

    Began mobile styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,10 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 
 // Logs message
 gulp.task('message', async() => {
     return console.log('Gulp is running...');
-});
-
-// Compile Sass to CSS into the src folder
-gulp.task('sass', async() => {
-    gulp.src('src/styles/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('src/styles/css'));
 });
 
 // Copy all HTML files to dist folder
@@ -21,6 +13,18 @@ gulp.task('copyHTML', async() => {
         .pipe(gulp.dest('dist'));
     gulp.src('src/snippets/*.html')
         .pipe(gulp.dest('dist/snippets'));
+});
+
+// Copy CSS file to dist folder
+gulp.task('moveCSS', async() => {
+    gulp.src('src/styles/css/*.css')
+        .pipe(gulp.dest('dist/css'));
+});
+
+// Move asset folder to dist folder
+gulp.task('moveAssets', async() => {
+    gulp.src('src/assets/*')
+        .pipe(gulp.dest('dist/assets'));
 });
 
 // Copy all HTML page snippets to dist folder
@@ -37,19 +41,13 @@ gulp.task('minifyJS', async() => {
         .pipe(gulp.dest('dist/js'))
 });
 
-// Minify CSS to dist folder
-gulp.task('minifyCSS', async() => {
-    gulp.src('src/styles/css/*.css')
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('dist/css'))
-});
 
-gulp.task('default', gulp.parallel(['message','sass','copyHTML','copyHTMLSnip','minifyJS','minifyCSS']));
+gulp.task('default', gulp.parallel(['message','copyHTML','copyHTMLSnip','minifyJS','moveCSS', 'moveAssets']));
 
 gulp.task('watch', async() => {
-    gulp.watch('src/styles/*.scss', gulp.parallel(['sass']));
     gulp.watch('src/*.html', gulp.parallel(['copyHTML']));
     gulp.watch('src/snippets/*.html', gulp.parallel(['copyHTMLSnip']));
     gulp.watch('src/js/*.js', gulp.parallel(['minifyJS']));
-    gulp.watch('src/styles/css/*.css', gulp.parallel(['minifyCSS']));
+    gulp.watch('src/styles/css/*.css', gulp.parallel(['moveCSS']));
+    gulp.watch('src/assets', gulp.parallel(['moveAssets']));
 });
